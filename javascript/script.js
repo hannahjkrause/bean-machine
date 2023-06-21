03f1819a13236c054f0dc75a241e5dd1b83cfb70
hannahjkrause <129633242+hannahjkrause@users.noreply.github.com>
2023-06-20 22:17:58 -0400

    Uploaded files

---
+++ javascript/script.js
@@ -0,0 +1,23 @@
+const beanOptions = [
+    {
+        id: 1,
+        img: 'assets\blonde-roast-lukas-669161.jpg',
+        name: 'Costa Rica',
+        roast: 'Light',
+        price: 21
+    },
+    {
+        id: 2,
+        img: 'assets\light-roast-pixabay-209476.jpg',
+        name: 'Brazilian Roast',
+        roast: 'Medium',
+        price: 24
+    },
+    {
+        id: 3,
+        img: 'assets\dark-roast-igor-haritanovich-1695052.jpg',
+        name: 'Columbian Roast',
+        roast: 'Dark',
+        price: 28
+    }
+]
